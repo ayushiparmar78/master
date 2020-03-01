a5ca0c1dde47051945bb87bd5b0afa38afc8ff0d
Ayushi Parmar <ayushiparmar78@gmail.com>
2020-03-01 19:23:57 +0530

    work done till wow

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+// require("jquery.counterup")
 
 import 'jquery'
 import 'popper.js'
 import 'bootstrap'
+import 'jquery.easing'
+// import 'jquery.counterup'
 
+require('packs/mainapp')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
